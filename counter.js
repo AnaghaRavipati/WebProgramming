3b5c83852cc6d99ea083c96b40b0065137922b70
AnaghaRavipati <167839755+AnaghaRavipati@users.noreply.github.com>
2024-04-25 23:02:19 +0530

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom'
+function Counter() {
+const [count, setCount] = useState(0);
+const incrementCount = () => setCount(count + 1);
+useEffect(() => {
+document.title = `You clicked ${count} times`
+});
+return (
+<div>
+<p>You clicked {count} times</p>
+<button onClick={incrementCount}>Increment counter </button>
+</div>
+)
+}
+export default Counter;
